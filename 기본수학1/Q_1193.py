110811842604f7790f7a381de951abbb0d814353
SeungGyu <tmdrb0912@naver.com>
2021-01-18 23:55:30 +0900

    년도 수정

---
+++ 기본수학1/Q_1193.py
@@ -0,0 +1,21 @@
+"""
+프로그래머 : Kwaktisu
+소속 : KNU CSE
+when : 21-01-18
+문제 번호 : 1193, 제목 : 분수찾기
+"""
+
+X = int(input())
+line = 1
+while X > line:
+    X -= line
+    line += 1
+
+if line % 2 == 0:
+    a = X
+    b = line - X + 1
+else:
+    a = line - X+1
+    b = X
+
+print(a, '/', b, sep='')
